c5687364317eee6b7b478e43dea979ca831f236a
ilie207 <ilusca.coroban@gmail.com>
2023-11-16 12:59:31 +0000

    Connection successful

--- .ccls-cache/@home@runner@Advanced-Program-in-C/AdvanceProgramming_wk2@main.cpp
+++ .ccls-cache/@home@runner@Advanced-Program-in-C/AdvanceProgramming_wk2@main.cpp
@@ -3,4 +3,5 @@
 int main() {
   std::cout << "Hello World!\n";
   std::cout << "Checking connection of github repository with replit.";
+  std::cout << "Checking connection of github repository with replit.";
 }
